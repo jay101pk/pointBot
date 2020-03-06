52a9ef88d35b7e3d477c4dee9ef5bfca8eebc600
jlogerqu <jason.logerquist@adtran.com>
2020-03-06 14:21:03 -0600

    Clean up files

--- plugins/pointBot.py
+++ plugins/pointBot.py
@@ -2,13 +2,8 @@ from sopel import module
 from sopel.tools import Identifier
 
 from datetime import datetime
-
+import re
 pluginName = "pointBot"
-
-autoMode = False
-userList = []
-ignoreList = []
-botList = []
 
 startHour = 8
 stopHour = 17
@@ -35,7 +30,7 @@ def updateGameRunning(bot):
     if not gameRunning and workhour:
         setGameRunning(bot.db, pluginName, True)
         resetGame(bot.db, bot.users)
-    time = timeExpr.match(str(datetime.now().time()))
+    time = re.match(timeExpr, str(datetime.now().time()))
     hour = int(time.group(1))
     setLastHour(bot.db, pluginName, hour)
 
@@ -50,22 +45,29 @@ def addGPoints(bot, trigger):
             bot.reply("The game is not running right now")
             return
         groups = trigger.groups()
-        amount = int(groups[1])
+        print(groups)
+        points = int(groups[1])
+        if groups[0] == '-':
+            amount = -points
+        else:
+            amount = points
         if len(groups) == 4:
             user = Identifier(groups[3])
         else:
             user = Identifier(groups[2])
 
         players = getPlayers(bot.db, bot.users)
-        buser = players.get(user)
-        if buser is None:
-            bot.reply("That is not a player")
+        buser = players.count(user)
+        if buser == 0:
+            bot.say("That is not a player", trigger.nick)
             return
         gpts = getgpts(bot.db, trigger.nick)
-        if gpts is None or abs(amount) > gpts:
-            bot.reply("You don't have enough gift points")
+        if gpts is None or points > gpts:
+            bot.say("You don't have enough gift points", trigger.nick)
             return
+        print(amount)
         addpts(bot.db, user, amount)
+        addgpts(bot.db, trigger.nick, -points)
 
 @module.nickname_commands(r'help')
 def help(bot, trigger):
@@ -89,7 +91,7 @@ def displaypoints(bot, trigger):
     prints out the points scoreboard
     """
     players = getPlayers(bot.db, bot.users)
-    print(players)
+    # print(players)
     ptsstring = displayPoints(bot.db, players)
     bot.say(ptsstring)
 
@@ -115,7 +117,7 @@ def setptscommand(bot, trigger):
     except ValueError:
         bot.reply("Bad number")
         return
-    buser = getUserFromUsers(args[0])
+    buser = getUserFromUsers(args[0], bot.users)
     if buser is not None:
         setpts(bot.db, buser.nick, args[1])
     elif args[0].lower() == "all":
@@ -137,7 +139,7 @@ def setgptscommand(bot, trigger):
     except ValueError:
         bot.reply("Bad number")
         return
-    buser = getUserFromUsers(args[0])
+    buser = getUserFromUsers(args[0], bot.users)
     if buser is not None:
         setgpts(bot.db, buser.nick, args[1])
     elif args[0].lower() == "all":
@@ -160,7 +162,7 @@ def setbotcommand(bot, trigger):
     user = getUserFromUsers(args[0], bot.users)
     isbot = args[1].lower() in ['true', '1', 't', 'y', 'yes']
     if user is not None:
-        setUserBotStatus(bot.db, user.nick, isbot)
+        setUserBotStatus(bot.db, user.nick, str( isbot ))
 
 @module.require_admin()
 @module.nickname_commands(r'ignore')
@@ -174,7 +176,7 @@ def setignorecommand(bot, trigger):
         return
     user = getUserFromUsers(args[0], bot.users)
     if user is not None:
-        setUserIgnoreStatus(bot.db, user.nick, True)
+        setUserIgnoreStatus(bot.db, user.nick, "True")
 
 @module.require_admin()
 @module.nickname_commands(r'unignore')
@@ -188,21 +190,41 @@ def setunignorecommand(bot, trigger):
         return
     user = getUserFromUsers(args[0], bot.users)
     if user is not None:
-        setUserIgnoreStatus(bot.db, user.nick, False)
+        setUserIgnoreStatus(bot.db, user.nick, "False")
+
+@module.nickname_commands(r'status')
+def statuscommand(bot, trigger):
+    """
+    Get status of user or other user if admin
+    """
+    if getUserIgnoreStatus(bot.db, trigger.nick) == "True":
+        return
+    if trigger.group(2) is None or not trigger.admin:
+        userpts = getpts(bot.db, trigger.nick)
+        usergpts = getgpts(bot.db, trigger.nick)
+        bot.say("You have {} points and {} gift points left".format(userpts, usergpts), trigger.nick)
+    else:
+        args = trigger.group(2).split()
+        user = args[0]
+        players = getPlayers(bot.db, bot.users)
+        if user not in players:
+            bot.reply("Invalid name")
+        else:
+            userpts = getpts(bot.db, user)
+            usergpts = getgpts(bot.db, user)
+            bot.say("{} has {} points and {} gift points left".format(user, userpts, usergpts), trigger.nick)
 
 
 """
 DB helper functions
 """
 def displayPoints(db, users):
-    returnString = "Here is a list of points in the format 'User: Total Points' (* = active)\n"
-    # pointlist = getpointList()
-    print(users)
+    returnString = "Here is a list of points in the format 'User: Total Points' (* = active) "
     for user in users:
         pts = getpts(db, user)
         returnString += "|{}|: {}, ".format(user, pts)
-        print(user, getgpts(db, user))
-    return returnString
+        print(user, "gpts:", getgpts(db, user))
+    return returnString[:-2]
 
 def resetGame(db, users):
     for user in users:
@@ -224,6 +246,10 @@ def getgpts(db, user):
 def setgpts(db, user, gpts):
     db.set_nick_value(user, "gpts", gpts)
 
+def addgpts(db, user, gpts):
+    cgpts = getgpts(db, user)
+    setgpts(db, user, gpts+cgpts)
+
 def getGameRunning(db, plugin):
     return db.get_plugin_value(plugin, "gamerunning", False)
 
@@ -240,13 +266,13 @@ def getUserFromUsers(nick, users):
     return users.get(Identifier(nick))
 
 def getUserBotStatus(db, user):
-    return db.get_nick_value(user, "bot", False)
+    return db.get_nick_value(user, "bot", "False")
 
 def setUserBotStatus(db, user, status):
     db.set_nick_value(user, "bot", status)
 
 def getUserIgnoreStatus(db, user):
-    return db.get_nick_value(user, "ignore", False)
+    return db.get_nick_value(user, "ignore", "False")
 
 def setUserIgnoreStatus(db, user, status):
     db.set_nick_value(user, "ignore", status)
@@ -254,11 +280,10 @@ def setUserIgnoreStatus(db, user, status):
 def getPlayers(db, users):
     players = []
     for user in users:
-        print(user, getUserBotStatus(db, user), getUserIgnoreStatus(db, user))
-        isplayer =  (getUserBotStatus(db, user) or getUserIgnoreStatus(db, user))
-        print(isplayer)
+        isbot = getUserBotStatus(db, user) == "True"
+        isignored = getUserIgnoreStatus(db, user) == "True"
+        isplayer = not(isbot or isignored)
         if isplayer:
             players.append(user)
-            print(user)
     return players
 
